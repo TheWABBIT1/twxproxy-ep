cffe45669839cb243a0deb59651b01697afbd542
donald.brown <donald.brown@c0b855e1-0b32-0410-92b9-556d6036b9be>
2008-09-06 14:17:51 +0000

    Playing with a wormhole script, adding better ansi support

--- twxbbs/scripts/game-A/stop-attacks.js
+++ twxbbs/scripts/game-A/stop-attacks.js
@@ -9,7 +9,7 @@ function attack(prompt) {
     match = prompt.match(attackPtn);
     if (match != null) {
         println("Try to attack "+match[1]+" with "+match[4]+" using "+match[3]+" fighters");
-        player.send("*\u001b[31m\u001b[1mLet's just be friends\u001b[22m*");
+        player.send("*#[bold;fgRed]Let's just be friends#[normal]*");
         game.setCapturingTextLineTrigger("no", "No", function(txt){});
         game.send("n");
 
